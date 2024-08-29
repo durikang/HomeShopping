e37a64a36272ef62347e4303553e338b39b7e121
maro <gksmfakt9@gmail.com>
2024-08-29 17:31:33 +0900

    08-29 카테고리 수정 절반까지

--- Admin/src/main/java/com/global/product/controller/ProductContentCategoryAction.java
+++ Admin/src/main/java/com/global/product/controller/ProductContentCategoryAction.java
@@ -1,31 +1,32 @@
 package com.global.product.controller;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.global.action.Action;
-import com.global.action.ActionForward;
 import com.global.action.View;
 import com.global.product.model.ProductCategoryDAO;
 import com.global.product.model.ProductCategoryDTO;
 
-public class ProductModifyCategoryAction implements Action {
+public class ProductContentCategoryAction implements Action {
 
 	@Override
 	public View execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
 		String category_no = request.getParameter("no").trim(); 
-				
+		
 		ProductCategoryDAO dao = ProductCategoryDAO.getInstance();
 		
-		ProductCategoryDTO dto = dao.getCategoryContent(category_no);
+		// 조회하는 메서드 호출
+		ProductCategoryDTO cont = dao.getCategoryContent(category_no);
 		
-		request.setAttribute("Modify", dto);
+		request.setAttribute("Content", cont);
 		
-		return new View("main.go").setUrl("/views/product/productCategoryModify.jsp");
+		
+		
+		
+		return new View("main.go").setUrl("/views/product/productCategoryContent.jsp");
 	}
 
 }
