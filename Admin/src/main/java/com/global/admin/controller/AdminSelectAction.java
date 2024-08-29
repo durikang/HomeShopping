43bc78d02b66199521b1f7e643647f2efff3fd24
CAD_11 <CAD_11@DESKTOP-766PNPQ>
2024-08-29 17:32:07 +0900

    Admin의 DAO, dTO 제작과 그 외의 필요한 폴더와 파일들 제작

---
+++ Admin/src/main/java/com/global/admin/controller/AdminSelectAction.java
@@ -0,0 +1,19 @@
+package com.global.admin.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.global.action.Action;
+import com.global.action.View;
+import com.global.admin.model.AdminDAO;
+
+public class AdminSelectAction implements Action {
+
+	@Override
+	public View execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		return null;
+	}
+}
