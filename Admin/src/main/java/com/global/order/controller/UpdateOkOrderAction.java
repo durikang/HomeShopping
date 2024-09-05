923b2413a79ea440314840bea97e2cff886875bd
gyeong seok <rudtjr134679@naver.com>
2024-09-05 17:39:16 +0900

    배송 상태 수정중

--- Admin/src/main/java/com/global/order/controller/UpdateOkOrderAction.java
+++ Admin/src/main/java/com/global/order/controller/UpdateOkOrderAction.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.global.action.Action;
 import com.global.action.View;
+import com.global.order.model.OrderDAO;
+import com.global.order.model.OrderDTO;
+import com.global.utils.ScriptUtil;
 
 public class UpdateOkOrderAction implements Action {
 
@@ -16,9 +19,26 @@ public class UpdateOkOrderAction implements Action {
 		int order_no = Integer.parseInt(request.getParameter("order_no").trim());
 		String status = request.getParameter("status");
 		int user_no = Integer.parseInt(request.getParameter("user_no").trim());
-		Date order_date = Daterequest.getParameter("order_date").trim();
 		int total_amount = Integer.parseInt(request.getParameter("total_amount").trim());
+		
+		OrderDTO dto = new OrderDTO();
+		
+		dto.setOrder_no(order_no);
+		dto.setStatus(status);
+		dto.setUser_no(user_no);
+		dto.setTotal_amount(total_amount);
+		
+		OrderDAO dao = OrderDAO.getInstance();
+		
+		int check = dao.ModifyOrder(dto);
+		
+		if (check > 0) {
+			ScriptUtil.sendScript(response, "배송 상태 수정 성공", "order_list.do");
+		} else {
+			ScriptUtil.sendScript(response, "배송 상태 수정 실패!!!", null);
+		}
 		return null;
+		
 	}
 
 }
